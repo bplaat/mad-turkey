93d5eeda7cbca66c57f72cd5d70d1fea48242742
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2019-11-21 09:53:46 +0100

    Add api send massurement support and some bug fixes

--- server/controllers/ApiController.php
+++ server/controllers/ApiController.php
@@ -2,6 +2,14 @@
 
 class ApiController {
     public static function sendMeasurement () {
-        // Do something with data
+        $station = Stations::select([ 'key' => $_POST['key'] ]);
+        Measurement::insert([
+            'station_id' => $station,
+            'time' => date('Y-m-d H:i:s'),
+            'temperature' => $_POST['temperature'],
+            'humidity' => $_POST['humidity'],
+            'light' => $_POST['light']
+        ]);
+        echo json_encode([ 'message' => 'Succesful' ]);
     }
 }
