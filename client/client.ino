5db2192cbcb143f290928dea2d27180ac3474881
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2019-11-27 11:22:27 +0100

    Update code and fritzing

--- client/client.ino
+++ client/client.ino
@@ -4,21 +4,23 @@
 #include <ArduinoJson.h>
 #include "config.h"
 
-#define MEASUREMENT_INTERVAL 60 * 1000
+#define EVENT_TYPE_LED 1
+#define EVENT_TYPE_BEEPER 2
 
 #define DHT_TYPE DHT11
 #define DHT_PIN D1
-#define LED_PIN D2  
-#define BUZZER_PIN D3
-#define LDR_PIN A0
-
 DHT dht(DHT_PIN, DHT_TYPE);
 
-const size_t capacity = 256;
-DynamicJsonDocument json_doc(capacity);
-int duration_led;
-int duration_buzzer;
-int freq_buzzer;
+#define LED_PIN D2
+uint32_t led_time = millis();
+uint32_t led_duration;
+#define BEEPER_PIN D3
+#define LDR_PIN A0
+
+#define MEASUREMENT_INTERVAL 60 * 1000
+uint32_t send_time = millis();
+
+DynamicJsonDocument json_document(256);
 
 String send_measurement() {
     String json;
@@ -30,7 +32,7 @@ String send_measurement() {
     http.begin(mad_turkey_api_url + "?key=" + mad_turkey_api_key + "&temperature=" + String(temperature) + "&humidity=" + String(humidity) + "&light=" + String(light), mad_turkey_api_https_fingerprint);
     int httpCode = http.GET();
     if (httpCode == HTTP_CODE_OK) {
-        json = http.getString();    //json object
+        json = http.getString();
         Serial.print("HTTP request response: ");
         Serial.println(http.getString());
     } else {
@@ -38,48 +40,43 @@ String send_measurement() {
         Serial.println(http.errorToString(httpCode));
     }
     http.end();
+
     return json;
 }
 
-void retrieve_event(String json) {
-    duration_led = 0;
-    duration_buzzer = 0;
-    freq_buzzer = 0;
+void retrieve_events(String json) {
+    led_duration = 0;
+    uint32_t beeper_duration = 0;
+    uint32_t beeper_frequency = 0;
 
-    DeserializationError error = deserializeJson(json_doc, json);
-
-    // Test if parsing succeeds.
+    DeserializationError error = deserializeJson(json_document, json);
     if (error) {
-        Serial.print(F("deserializeJson() failed: "));
+        Serial.print("deserializeJson() failed: ");
         Serial.println(error.c_str());
         return;
     }
 
-    const char* message = json_doc["message"];
-    Serial.println(message);
-    
-    JsonArray events = json_doc["events"];
-
+    JsonArray events = json_document["events"];
     for (uint8_t i = 0; i < events.size(); i++) {
-        if (events[i]["type"] == 0) {
-        duration_led = events[i]["duration"];
+        JsonObject event = events[i];
+        if (event["type"] == EVENT_TYPE_LED) {
+            led_duration = event["duration"];
         }
 
-        if (events[i]["type"] == 1) {
-        duration_buzzer = events[i]["duration"];
-        freq_buzzer = events[i]["frequency"];
+        if (event["type"] == EVENT_TYPE_BEEPER) {
+            beeper_duration = event["duration"];
+            beeper_frequency = event["frequency"];
         }
     }
 
-    if (duration_led > 0) {
+    if (led_duration > 0) {
         digitalWrite(LED_PIN, HIGH);
-        uint32_t start_time = millis();
+        led_time = millis();
     }
 
-    if (duration_buzzer > 0 && freq_buzzer > 0) {
-        tone(BUZZER_PIN, freq_buzzer, duration_buzzer);
+    if (beeper_duration > 0 && beeper_frequency > 0) {
+        tone(BEEPER_PIN, beeper_frequency, beeper_duration);
     }
-
 }
 
 void setup() {
@@ -97,16 +94,14 @@ void setup() {
     retrieve_event(send_measurement());
 }
 
-uint32_t send_time = millis();
-uint32_t start_time = millis();
 void loop() {
     if (millis() - send_time > MEASUREMENT_INTERVAL) {
         send_time = millis();
-        retrieve_event(send_measurement());
+        retrieve_events(send_measurement());
     }
 
-    if (millis() - start_time > duration_led) {
-      start_time = millis();
-      digitalWrite(LED_PIN, LOW);
+    if (millis() - led_time > led_duration) {
+        led_time = millis();
+        digitalWrite(LED_PIN, LOW);
     }
 }
